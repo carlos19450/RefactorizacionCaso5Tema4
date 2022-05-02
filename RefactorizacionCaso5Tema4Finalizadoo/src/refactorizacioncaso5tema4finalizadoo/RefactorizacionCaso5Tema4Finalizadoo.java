a349e2cd4a35a800bebd397cb2a42409e2d03b2a
carpui <carpui@ubuntu>
2022-05-02 17:07:28 +0200

    Ejercicio de refactorización y javadoc terminado.

---
+++ RefactorizacionCaso5Tema4Finalizadoo/src/refactorizacioncaso5tema4finalizadoo/RefactorizacionCaso5Tema4Finalizadoo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package refactorizacioncaso5tema4finalizadoo;
+
+/**
+ *
+ * @author carpui
+ */
+public class RefactorizacionCaso5Tema4Finalizadoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
