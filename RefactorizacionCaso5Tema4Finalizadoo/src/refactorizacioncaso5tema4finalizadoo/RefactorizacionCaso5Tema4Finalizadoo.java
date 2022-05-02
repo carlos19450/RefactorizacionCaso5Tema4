e80e199b00ed39944516befa2c60e3a6245e51dc
carpui <carpui@ubuntu>
2022-05-02 17:11:33 +0200

    Clase main terminada.

--- RefactorizacionCaso5Tema4Finalizadoo/src/refactorizacioncaso5tema4finalizadoo/RefactorizacionCaso5Tema4Finalizadoo.java
+++ RefactorizacionCaso5Tema4Finalizadoo/src/refactorizacioncaso5tema4finalizadoo/RefactorizacionCaso5Tema4Finalizadoo.java
@@ -1,20 +1,38 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Paso 1: Organizar el siguiente código en un proyecto Java.
+ * Paso 2: Dar formato al código.
+ * Paso 3: Subir el proyecto a GitHub.
+ * Paso 4: Documentar para Javadoc.
+ * Paso 5: Refactorizar.
  */
+
 package refactorizacioncaso5tema4finalizadoo;
 
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 /**
  *
  * @author carpui
  */
 public class RefactorizacionCaso5Tema4Finalizadoo {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+//MatriculaDB matriculaDB = new MatriculaDB();
+//Matricula matricula = matriculaDB.getMatricula(2);
+// TODO: pasar parámetros al contructor de Matricula
+        ArrayList listaAsignaturas = new ArrayList();
+        Estudiante alumno1 = new Estudiante("Pepe", "Roberto", "20463856G");
+        Assignatura asig1 = new Assignatura(4321, "Programacion", 8, 2, true);
+        Assignatura asig2 = new Assignatura(4444, "Entornos", 4, 1, true);
+        listaAsignaturas.add(asig1);
+        listaAsignaturas.add(asig2);
+        Matricula matricula1 = new Matricula(1234, 2, listaAsignaturas);
+
+        System.out.println(alumno1.toString());
+        System.out.print("Cost de la matricula " + matricula1.costMatricula() + "\n");
     }
-    
 }
+
